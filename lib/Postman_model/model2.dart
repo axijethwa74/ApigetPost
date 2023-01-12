0bf04d4b7e56ec3ac8ecc04025ca3799d76951d5
Yougesh Jethwa <105840649+axijethwa74@users.noreply.github.com>
2023-01-12 16:44:19 +0500

    saves

---
+++ lib/Postman_model/model2.dart
@@ -0,0 +1,12 @@
+class Model2 {
+  String title;
+  String url;
+  int id;
+  String thumbnailUrl;
+
+  Model2(
+      {required this.title,
+      required this.url,
+      required this.thumbnailUrl,
+      required this.id});
+}
